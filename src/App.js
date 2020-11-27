f9f175edca09f38fa2510759344b12d0efd244f0
SorinCho <sorincho@gmail.com>
2020-11-27 13:28:05 -0500

    basic auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Habits from './pages/Habits';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/habits">
+            <Habits />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Habits.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Habits() {
+  return (<div class="container">
+  <div id="login">
+    <h1>Habits page</h1>
+  </div>
+</div>);
+}
+
+export default Habits;
