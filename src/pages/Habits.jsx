db5c40335008a07764ccb11c350727c96a961125
SorinCho <sorincho@gmail.com>
2020-12-12 18:11:59 -0500

    handle eslint warnings

--- src/pages/Habits.jsx
+++ src/pages/Habits.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 function Habits() {
-  return (<div class="container">
-  <div id="login">
-    <h1>Habits page</h1>
-  </div>
-</div>);
+  return (
+    <div className="container">
+      <div id="login">
+        <h1>Habits page</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Habits;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 
 function Login() {
-  return (<div class="container">
-  <div id="login">
-    <h1>This is an example of the Authorization Code flow</h1>
-    <a href="/login" class="btn btn-primary">Log in with Spotify</a>
-  </div>
-  {/* <div id="loggedin">
+  return (
+    <div className="container">
+      <div id="login">
+        <h1>This is an example of the Authorization Code flow</h1>
+        <a href="/login" className="btn btn-primary">
+          Log in with Spotify
+        </a>
+      </div>
+      {/* <div id="loggedin">
     <div id="user-profile">
     </div>
     <div id="oauth">
     </div>
-    <button class="btn btn-default" id="obtain-new-token">Obtain new token using the refresh token</button>
+    <button class="btn btn-default" id="obtain-new-token">
+    Obtain new token using the refresh token</button>
   </div> */}
-</div>);
+    </div>
+  );
 }
 
 export default Login;
