d6867298c36bb0554e12f645ae1351fe2d1e3ead
SorinCho <sorincho@gmail.com>
2021-01-04 16:20:58 -0500

    rounding

--- src/services/calculations.js
+++ src/services/calculations.js
@@ -1,11 +1,15 @@
+function round(num) {
+  return Math.round((num + Number.EPSILON) * 100) / 100;
+}
+
 export function avgDuration(tracks) {
-  const total = tracks.reduce((sum, { duration_ms }) => sum + duration_ms, 0);
-  return total / tracks.length;
+  const total = tracks.reduce((sum, { duration_ms }) => sum + duration_ms, 0); //eslint-disable-line
+  return round(total / tracks.length / 60000);
 }
 
 export function pctExplicit(tracks) {
-  const total = tracks.filter((track) => track.explicit == true).length;
-  return total / tracks.length;
+  const total = tracks.filter((track) => track.explicit === true).length;
+  return round(total / tracks.length);
 }
 
 export function avgFollowers(artists) {
@@ -13,12 +17,12 @@ export function avgFollowers(artists) {
     (sum, { followers }) => sum + followers.total,
     0
   );
-  return total / artists.length;
+  return round(total / artists.length);
 }
 
 export function avgPopularity(artists) {
   const total = artists.reduce((sum, { popularity }) => sum + popularity, 0);
-  return total / artists.length;
+  return round(total / artists.length);
 }
 
 export function aggGenres(artists) {
@@ -29,9 +33,11 @@ export function aggGenres(artists) {
     });
   });
   const sortable = [];
-  for (const genre in counts) {
-    sortable.push([genre, counts[genre]]);
-  }
+  // counts.forEach((genre) => sortable.push([genre, counts[genre]]));
+  Object.keys(counts).forEach((genre) => sortable.push([genre, counts[genre]]));
+  // for (const genre in Object.keys(counts)) {
+  //   sortable.push([genre, counts[genre]]);
+  // }
   sortable.sort((a, b) => b[1] - a[1]);
   return sortable;
 }
