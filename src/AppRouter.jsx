26ffbc925ab416a1d215851eeeb8c13db388172e
SorinCho <sorincho@gmail.com>
2021-01-04 18:50:03 -0500

    add login page and authenticated hooks

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Header from './components/Header';
 
 export default function AppRouter() {
+  const [authenticated, setAuthenticated] = useState(false);
   return (
     <Router>
-      <div>
+      <div className="page">
         <Switch>
+          <Route path="/home">
+            <Home
+              handleNotAuthenticated={() => setAuthenticated(false)}
+              handleAuthenticated={() => setAuthenticated(true)}
+            />
+          </Route>
           <Route path="/">
-            <Home />
+            <Login />
           </Route>
         </Switch>
+        <Header
+          authenticated={authenticated}
+          handleNotAuthenticated={() => setAuthenticated(false)}
+        />
       </div>
     </Router>
   );
